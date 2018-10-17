0d2776a74c52285198a9603e2b1c5bed0247676f
Gordan Graberec <gordan.graberec@minus5.hr>
2018-10-17 16:41:02 +0200

    fix file fetching

--- src/components/detail-view-rendering-kit/File.js
+++ src/components/detail-view-rendering-kit/File.js
@@ -1,19 +1,31 @@
 
-import React, { PureComponent } from 'react';
+import React, { Component } from 'react';
 import * as fileRenderingKit from './file-rendering-kit';
 
-export class File extends PureComponent {
+export class File extends Component {
   state = { content: '' }
-  get content() {
-    const { content } = this.state;
-    if (content) return content;
+  loadContent() {
     const { node } = this.props;
-    if (node.content) return node.content;
     if (node.filePath) {
       fetch(node.filePath)
         .then(res => res.text())
         .then(text => this.setState({ content: text.toString() }))
     }
+  }
+  componentDidMount() {
+    this.loadContent();
+  }
+  componentDidUpdate(nextProps) {
+    if (this.props.node !== nextProps.node) {
+      this.setState({ content: '' });
+      this.loadContent();
+    }
+  }
+  get content() {
+    const { content } = this.state;
+    if (content) return content;
+    const { node } = this.props;
+    if (node.content) return node.content;
     return '';
   }
   render() {
